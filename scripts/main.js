9a0eb33ad111974eeaf4f286fe0dffff2b406038
axelviskovic <47737750+axelviskovic@users.noreply.github.com>
2024-03-03 12:45:31 +0100

    t
    t

---
+++ scripts/main.js
@@ -0,0 +1,576 @@
+// Questions
+
+data = [
+'Qui ici aimerais-tu pécho ?',
+
+'Quelle personnalité devrait devenir président de la république française selon toi ?',
+
+'quel est le meilleur film qui existe ?',
+
+'Quel est ton plat préféré ?',
+
+'Quelle est ta date de naissance ?',
+
+'Si tu pouvais baiser une personne sur cette planète qui serait-ce ?',
+
+'Tu as adopté un chien, comme l’appelles-tu ?',
+
+'Si tu pouvais te renommer, comment t’appellerais-tu ?',
+
+'Qu’est ce que tu bois comme mélange / marque d’alcool en soirée ?',
+
+'Quel prix max mettrais-tu pour des chaussures ?',
+
+'Quel est ton artiste préféré?',
+
+'Quel est le pays ou tu aurais voulu vivre ?',
+
+'Quelle est ta couleur préférée ?',
+
+'Quelles ont été tes meilleures vacances ?',
+
+'Quelle est ta position sexuelle préférée ?',
+
+'Quelle est ta série préférée ?',
+
+'Combien de fois as tu vomi cette année?',
+
+'Quel est ton deuxième prénom?',
+
+'Quel est ton plat préféré',
+
+'Combien pèses-tu?',
+
+'Quelle est ta taille?',
+
+'Qu’as-tu fais de beau hier ?',
+
+'Si tu pouvais avoir un super pouvoir quel serait-il ?',
+
+'Quelle est la durée max que tu aies faite sans dormir ?',
+
+'Combien as tu eu de partenaires sexuels',
+
+'À quel âge as-tu fais ta première fois ?',
+
+'Comment s’appelle le nom de ta rue?',
+
+'Quel est le plus haut diplôme que tu aies obtenu ?',
+
+'Quel est ton mot / ta phrase fétiche ?',
+
+'Quel est ton animal totem ?',
+
+'Quel âge avait la personne la plus jeune avec qui tu aies couché ?',
+
+'Quel âge avait la personne la plus vieille avec qui tu aies couché ?',
+
+'Qui rêves tu de voir en concert / festival ?',
+
+'Comment s’appelle la dernière personne avec qui tu aies eu un rapport sexuel ?',
+
+'Combien de drogues (alcool compris, café/sucre pas compris) as-tu essayé dans ta vie ?',
+
+'À quand remonte ta dernière cuite ? (Approximativement)',
+
+'Où est tu allé pour tes dernières vacances ?',
+
+'Avec qui ferais tu une coloc dans cette pièce ?',
+
+'Quel est le pays le plus lointain où tu es allé dans ta vie ?',
+
+'Décris ton métier d’une manière bizarre',
+
+'Quelle est la chose la plus hard que tu aies faite sexuellement ?',
+
+'Raconte une chose folle qui t’est arrivée',
+
+'Combien de personnes dans cette pièce as-tu déjà embrassé ?',
+
+'Quel est le prénom de ta daronne ?',
+
+'Quel est le prénom de ton daron ?',
+
+'À quelle heure mets tu ton réveil le matin en semaine ?',
+
+'Quel est ton fantasme ?',
+
+'À quand remonte ton dernier caca ?',
+
+"Comment s'apelle la première personne que tu aies embrassée ?",
+
+"Qui est ton amour le plus récent ?",
+
+'Quel est le dernier fantasme que tu aies réalisé ?',
+
+"Quel est ton métier de rêve ?",
+
+"Quel est l'endroit le plus fou où tu aies fait l'amour ?",
+
+"Quel est le dernier cadeau que tu aies offert à quelqu'un",
+
+"Quelle est la dernière série que tu aies regardée ?",
+
+"Quel est le dernier film que tu aies regardé ?",
+
+"À quand remonte la première fois que tu aies fumé ?",
+
+"Qui est le dernier artiste que tu aies vu en concert / festival ?",
+
+"Quel est le plus long trajet que tu aies fait dans ta vie ?",
+
+"Combien as-tu de frère et soeur ?",
+
+"Quel est l'artiste que tu aimes le moins ?",
+
+"Quelle personnalité connue détestes tu ?",
+
+"Qui est ton idole ?",
+
+"Qu'est ce que tu préfères chez toi physiquement ?",
+
+"Quel est ton plus gros défaut ?",
+
+"Quelle est ta plus grande qualité ?",
+
+"Quelles sont tes prochaines vacances prévues ?",
+
+"Que penses-tu de la situation économique en Argentine depuis la fin du 20ème siècle ?",
+
+"Si tu devais mettre une baffe à quelqu'un ici tu choisirais qui ?",
+
+"Qu'est ce que tu dirais à ton ex si tu le/la croisais ?",
+
+"Si tu pouvais voyager dans le temps, quelle époque visiterais-tu ?",
+
+"Quelle est ta chanson préférée ?",
+
+"Si tu pouvais rencontrer n'importe quelle personne, morte ou vivante, qui choisirais-tu ?",
+
+"Quelle est ta plus grande peur ?",
+
+"Quel est le trait de caractère que tu apprécies le plus chez les autres ?",
+
+"Si tu avais un jour de congé illimité, que ferais-tu ?",
+
+"Quelle est ta vision du paradis, ton monde idéal ?",
+
+"Quel est ton plus grand rêve que tu n'as pas encore réalisé ?",
+
+"Si tu pouvais avoir une compétence instantanément, laquelle serait-ce ?",
+
+"Quelle est ta pire addiction ?",
+
+"Quel est le plat que tu ne pourrais jamais manger ?",
+
+"Quel est le dernier film ou série que tu as binge-watché ?",
+
+"Si tu pouvais rencontrer un personnage fictif, qui serait-ce ?",
+
+"Quel est le meilleur conseil que tu donnerais à ton moi plus jeune ?",
+
+"Si tu devais créer une entreprise, quel serait le domaine d'activité ?",
+
+"Quelle est la ta meileure compétence ?",
+
+"Si tu étais une pizza, quelle serait ta garniture?",
+
+"Quel serait le titre de ton autobiographie",
+
+"Comment occupes-tu ton temps libre ?",
+
+"Quel est le meilleur conseil que tu aies jamais reçu ?",
+
+"Quelle est ta routine matinale typique ?",
+
+"Quelle est ta plus grande passion dans la vie ?",
+
+"Quelle est la personne qui t'inspire le plus ?",
+
+"Quel est le moment le plus mémorable de ta vie jusqu'à présent ?",
+
+"Quelle est la dernière chose qui t'a fait rire aux éclats ?",
+
+"Quel est ton endroit favori dans ta ville ?",
+
+"Quelle est la chose que tu apprécies le plus dans une amitié ?",
+
+"Si tu pouvais vivre dans n'importe quelle époque, laquelle choisirais-tu ?",
+
+"Si tu devais écrire un livre, de quoi ça parlerait ?",
+
+"Si tu pouvais changer une seule chose dans le monde, que serait-ce ?",
+
+"Quelle est la chanson qui te donne toujours envie de danser ?",
+
+"Si tu devais décrire ta vie en trois mots, quels seraient-ils ?",
+
+"Quel est ton plus grand accomplissement jusqu'à présent ?",
+
+"Quelle est la chose la plus étrange que tu aies mangée ?",
+
+"Quel est le premier site / première appli que tu consultes le matin ?",
+
+"Si tu pouvais être célèbre pour quelque chose, que serait-ce ?",
+
+"Quelle est la chose que tu voudrais accomplir dans les cinq prochaines années ?",
+
+"Si tu pouvais avoir une conversation avec ton moi du futur, que lui demanderais-tu ?"
+
+
+]
+
+
+
+
+// CREATION LISTE JOUEURS
+
+const buttonAddPlayer = document.querySelector('.buttonAddPlayer')
+const inputAddPlayer = document.querySelector('.inputAddPlayer')
+const containerlistPlayer = document.querySelector('.containerlistPlayer')
+let listeJoueur= []
+
+buttonAddPlayer.style.height=buttonAddPlayer.offsetWidth+"px"
+
+
+
+function addPlayer(){
+    playSound()
+    if(inputAddPlayer.value==''){
+        inputAddPlayer.classList.add('noNameInput')
+    }
+    else{
+        listeJoueur.push(inputAddPlayer.value)
+        inputAddPlayer.classList.remove('noNameInput')
+        createListJoueur(listeJoueur)  
+        inputAddPlayer.value=''  
+        inputAddPlayer.focus()    
+    }      
+}
+
+buttonAddPlayer.addEventListener('click',()=>{addPlayer()})
+
+
+function createListJoueur(liste){
+    document.querySelectorAll('.rowPlayerList').forEach((el)=>{el.remove()})
+    liste.forEach((joueur, i)=>{
+        let newDiv = document.createElement("div")
+        newDiv.classList.add('rowPlayerList')
+
+        let newPlayer = document.createElement("h2")
+        newPlayer.classList.add('subTitlePlayer')
+        newPlayer.innerText = joueur
+
+        newDiv.appendChild(newPlayer)        
+
+        containerlistPlayer.appendChild(newDiv)
+        sessionStorage.setItem('listeJoueur', JSON.stringify(listeJoueur))
+    })
+}
+
+
+/*if(sessionStorage.getItem('listeJoueur')){
+    listeJoueur=JSON.parse(sessionStorage.getItem('listeJoueur'))
+    createListJoueur(listeJoueur)
+}*/
+
+const sounds = document.querySelectorAll('.sound')
+const settingsContainer = document.querySelector('.settingsContainer')
+const nbQuestionCase = document.querySelectorAll('.nbQuestionCase')
+
+let soundOk = true
+let isLgbt = false
+
+nbQuestionCase.forEach((el)=>{
+    el.addEventListener('click',(event)=>{
+        if(!event.target.classList.contains('nbQuestionSelected')){
+            document.querySelector('.nbQuestionSelected').classList.remove('nbQuestionSelected')
+            el.classList.add('nbQuestionSelected')
+            setNbQuestion=el.getAttribute('data')
+        }
+    })
+})
+
+function playSound(){
+    if(soundOk==true){
+        const rand = Math.floor(Math.random() * sounds.length)
+        sounds[rand].play()
+    }
+}
+
+document.querySelector('#settingsOpen').addEventListener('mousedown',()=>{playSound();settingsContainer.style.display="flex"})
+document.querySelector('#settingsBtnClose').addEventListener('mousedown',()=>{playSound();settingsContainer.style.display="none"})
+
+croixRougeSon = document.querySelector('.croixRougeSon')
+croixRougeLgbt = document.querySelector('.croixRougeLgbt')
+
+document.querySelector('#sonSetting').addEventListener('mousedown',()=>{
+    playSound()
+    if(soundOk==true){
+        croixRougeSon.style.display="block"
+        soundOk=false
+    }
+    else if(soundOk==false){
+        croixRougeSon.style.display="none"
+        soundOk=true
+    }
+})
+
+document.querySelector('#lgbtIcon').addEventListener('mousedown',()=>{
+    playSound()
+    if(isLgbt==true){
+        croixRougeLgbt.style.display="block"
+        document.querySelector('body').style.background="linear-gradient(#7abddc, #357a94)"
+        isLgbt=false
+    }
+    else if(isLgbt==false){
+        croixRougeLgbt.style.display="none"
+        document.querySelector('body').style.background="linear-gradient(to right, violet, indigo, blue, green, yellow, orange, red)"
+        isLgbt=true
+    }
+})
+
+const buttonValidationPlayer = document.querySelector('.buttonValidationPlayer')
+const pagePlayerSelect = document.querySelector('.pagePlayerSelect')
+const fillQuestionScreen = document.querySelector('.fillQuestionScreen')
+const prenom = document.querySelector('.prenom')
+const lesgo = document.querySelector('.lesgo')
+const suivant = document.querySelector('.suivant')
+const selectPlayer = document.querySelector('.selectPlayer')
+const fillQuestion = document.querySelector('.fillQuestion')
+const showAnswersScreen = document.querySelector('.showAnswersScreen')
+const showQuestion = document.querySelector('#showQuestion')
+const showAnswer = document.querySelector('#showAnswer')
+const buttonAnswer = document.querySelector('.buttonAnswer')
+const absoluteAnswer = document.querySelector('.absoluteAnswer')
+const absoluteAnswerText = document.querySelector('#absoluteAnswerText')
+const absoluteShowBtn = document.querySelector('.absoluteShowBtn')
+const speak = document.querySelector('#speak')
+
+
+let listeQuestions = []
+let listeAnswers = []
+let randomAnswers = []
+let questionsFaites = []
+let seaki = 1
+let nbQuestion=1
+let nbCreateQuestion = 1
+let setNbQuestion = 3
+
+
+
+//function createAnswer(i){
+    class Answer {
+        constructor(question, reponse, joueur) {
+            this.question = document.querySelector('#question'+nbQuestion).textContent
+            this.reponse = document.querySelector('#answer'+nbQuestion).value
+            this.joueur = prenom.textContent
+        }
+    }
+    //return answer
+//}
+
+function createListQuestion(){
+    nbCreateQuestion=1
+    const oldDiv = document.querySelectorAll('.newDiv')
+    oldDiv.forEach((el)=>{el.remove()})
+    for(let i=0; i<setNbQuestion;i++){
+        let  inputQuestion = `<h2 id="question`+nbCreateQuestion+`" class="questionTitle"></h2>`
+        let createInput = `<input id="answer`+nbCreateQuestion+`" class="inputAddPlayer inGame" type="text">`
+        const newDiv = document.createElement('div')
+        newDiv.innerHTML=inputQuestion+createInput
+        newDiv.classList.add('newDiv')
+        fillQuestion.insertBefore(newDiv, suivant)
+        document.querySelector('#question'+(i+1)).textContent=listeQuestions[i+((seaki-1)*setNbQuestion)]
+        nbCreateQuestion++
+    }
+}
+
+function sayAnswer(question, answer){
+    let syntheseVocale = window.speechSynthesis;
+    let message = new SpeechSynthesisUtterance(question);
+    syntheseVocale.speak(message);
+    setTimeout(() => {
+        let syntheseVocale2 = window.speechSynthesis;
+        let message2 = new SpeechSynthesisUtterance(answer);
+        syntheseVocale2.speak(message2);
+      }, "3000");
+}
+
+function sayPrenom(prenom){
+    let syntheseVocale = window.speechSynthesis;
+    let message = new SpeechSynthesisUtterance(prenom);
+    syntheseVocale.speak(message); 
+}
+
+speak.addEventListener('click',()=>{
+    sayAnswer(document.querySelector('#showQuestion').textContent, document.querySelector('#showAnswer').textContent)
+})
+
+
+function answerThis(joueur){
+    prenom.textContent=listeJoueur[seaki-1]
+    //for(let i=0; i<4;i++){}
+    createListQuestion()
+    //document.querySelector('#question1').textContent=listeQuestions[0]
+    //listeQuestions.shift()
+    // document.querySelector('#question2').textContent=listeQuestions[0]
+    // listeQuestions.shift()
+    // document.querySelector('#question3').textContent=listeQuestions[0]
+    // listeQuestions.shift()
+    seaki++    
+}
+
+let tourAnswer = 0
+
+function montreReponses(){
+    if(tourAnswer==randomAnswers.length){  
+        document.querySelector('body').style.background="linear-gradient(#7abddc, #357a94)"
+        listeAnswers.length=0
+        randomAnswers.length=0
+        seaki=1
+        nbQuestion=1
+        tourAnswer=1
+        for(let i=0; i<listeJoueur.length; i++){
+            for(let i=0; i<setNbQuestion; i++){
+                let rand = Math.floor(Math.random() * data.length);
+                listeQuestions.push(data[rand])
+            }
+        }
+        showAnswersScreen.style.display="none"
+        selectPlayer.style.display="flex"
+        answerThis(listeJoueur[seaki-1])
+    }
+    else{
+        showQuestion.textContent=randomAnswers[tourAnswer].question
+        showAnswer.textContent='"'+randomAnswers[tourAnswer].reponse+'"'
+        sayAnswer(document.querySelector('#showQuestion').textContent, document.querySelector('#showAnswer').textContent)
+        tourAnswer++
+    }  
+}
+
+buttonAnswer.addEventListener('click',()=>{
+    absoluteAnswerText.textContent="Réponse : "+randomAnswers[tourAnswer-1].joueur
+    sayPrenom(randomAnswers[tourAnswer-1].joueur)
+    absoluteAnswer.style.display="flex"
+})
+
+absoluteShowBtn.addEventListener('click',()=>{
+    absoluteAnswer.style.display="none"
+    montreReponses()
+})
+
+function inputNoEmpty(){
+    let isGood = true
+    const inputActual = document.querySelectorAll('.inputAddPlayer.inGame')
+    inputActual.forEach((el)=>{
+        if(el.value==""){
+            isGood=false
+        }
+    })
+    return isGood
+}
+
+suivant.addEventListener('click',()=>{
+    const inputActual = document.querySelectorAll('.inputAddPlayer.inGame')
+    inputActual.forEach((el)=>{
+        el.classList.remove('noNameInput')
+    })
+    if(seaki>listeJoueur.length){
+        nbQuestion=1
+        for(let i=0;i<setNbQuestion;i++){
+            let newAnswer = new Answer
+            nbQuestion++
+            listeAnswers.push(newAnswer)
+        }
+        document.querySelector('body').style.background="linear-gradient(#f3aed9, #b10bb5)"
+        showAnswersScreen.style.display="flex"
+        fillQuestion.style.display="none"
+        prenom.textContent="Sékisa ?"
+        randomAnswers = [...listeAnswers];
+        for (let i = randomAnswers.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [randomAnswers[i], randomAnswers[j]] = [randomAnswers[j], randomAnswers[i]];
+        }
+        montreReponses()
+    }
+    else{ 
+        playSound()
+        if(inputNoEmpty()){
+            nbQuestion=1
+            for(let i=0;i<setNbQuestion;i++){
+                let newAnswer = new Answer
+                nbQuestion++
+                listeAnswers.push(newAnswer)
+            }
+            selectPlayer.style.display="flex"
+            fillQuestion.style.display="none"
+            answerThis(listeJoueur[seaki-1])
+        }
+        else{  
+            inputActual.forEach((el)=>{
+                if(el.value==""){
+                    el.classList.add('noNameInput')
+                }
+            })
+        }
+    }
+})
+
+
+// START GAME
+buttonValidationPlayer.addEventListener('click',()=>{
+    playSound()
+    inputAddPlayer.classList.remove('noNameInput')
+    if(listeJoueur.length>2){
+        pagePlayerSelect.style.display="none"
+        fillQuestionScreen.style.display="block"
+        for(let i=0; i<listeJoueur.length; i++){
+            questionsFaites.length=0
+            for(let i=0; i<setNbQuestion; i++){
+                let newQuestion 
+                do{
+                    let rand = Math.floor(Math.random() * data.length);
+                    newQuestion = data[rand]
+                }
+                while(questionsFaites.includes(newQuestion))
+                listeQuestions.push(newQuestion)
+                questionsFaites.push(newQuestion)
+                
+                }
+            }
+        answerThis(listeJoueur[seaki-1])
+    }
+    else{
+        inputAddPlayer.classList.add('noNameInput')
+    }
+})
+
+lesgo.addEventListener('click',()=>{
+    playSound()
+    selectPlayer.style.display="none"
+    fillQuestion.style.display="flex"
+    for(let i=0; i<setNbQuestion.length;i++){
+        document.querySelector('#answer'+(i+1)).value=""  
+    }
+})
+
+
+
+window.addEventListener('beforeunload', function (e) {
+    // Message de confirmation personnalisé
+    var confirmationMessage = 'Êtes-vous sûr de vouloir quitter la page ?';
+
+    // (Standard) Pour les navigateurs récents
+    if (typeof e === 'undefined') {
+        e = window.event;
+    }
+
+    // Pour les navigateurs modernes
+    if (e) {
+        e.returnValue = confirmationMessage;
+    }
+
+    // Pour les navigateurs obsolètes
+    return confirmationMessage;
+});
